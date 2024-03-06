c2bb3b87df3896f807f46a4baecdbac2bf386ef3
2salted <noamgraf123@gmail.com>
2024-03-06 16:23:44 -0100

    big update, new words

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { wpmTests } from "../words";
 
 export default function TextInput() {
+  
   function pickRandomArray(array: string[][]) {
     let randomIndex = Math.floor(Math.random() * array.length);
 
@@ -8,6 +10,13 @@ export default function TextInput() {
   }
 
   let pickedArray = pickRandomArray(wpmTests);
+  // let startingLetter = pickedArray[0][0];
+  let arrayIntoLetters = pickedArray
+    .toString()
+    .split("")
+    .filter((letter) => letter !== ",");
+
+  console.log(arrayIntoLetters);
 
   return (
     <>
@@ -15,13 +24,13 @@ export default function TextInput() {
         <div className="flex flex-col w-full lg:max-w-screen-xl">
           <div className="h-36 overflow-hidden">
             <div className="flex flex-wrap w-full pb-10">
-              {pickedArray.map((word, index) => (
+              {pickedArray.map((word, wordIndex) => (
                 <div
-                  className="flex text-2xl leading-loose text-neutral-500 pl-1 pr-1"
-                  key={index}
+                  key={wordIndex}
+                  className="flex text-2xl leading-loose text-neutral-500 pl-1 pr-2"
                 >
-                  {word.split("").map((letter, index) => (
-                    <div key={index}>{letter}</div>
+                  {word.split("").map((letter, letterIndex) => (
+                    <div key={letterIndex}>{letter}</div>
                   ))}
                 </div>
               ))}
@@ -30,7 +39,14 @@ export default function TextInput() {
         </div>
         <div className="w-full flex justify-center pt-5">
           <div>
-            <input className="bg-neutral-600 outline-none text-neutral-500"></input>
+            <input
+              className="bg-neutral-600 outline-none text-neutral-500"
+              onChange={(e) => {
+                if (e.target.value) {
+                  console.log(e.target.value);
+                }
+              }}
+            ></input>
           </div>
         </div>
       </div>
